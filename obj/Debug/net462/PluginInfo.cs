46df6e94dd396e2bd0e518f60ab3e535d4f68e45
NepuShiro <nepushiro@nepunep.xyz>
2023-10-15 20:36:24 -0500

    Delete obj directory

--- obj/Debug/net462/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RedirectLog
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "RedirectLog";
-        public const string PLUGIN_NAME = "RedirectLog";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
