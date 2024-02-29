cd32205f600562fbea3ebd8abb243c264337c82e
NepuShiro <nepushiro@nepunep.xyz>
2024-02-29 03:15:13 -0600

    Complete Refactor

--- Plugin.cs
+++ Plugin.cs
@@ -1,149 +1,177 @@
-﻿using BepInEx;
-using BepInEx.Configuration;
-using UnityEngine;
-using System;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
-using BCE;
+using BepInEx;
+using BepInEx.Configuration;
 using Elements.Core;
-using static OfficialAssets.Graphics;
-using static UnityEngine.UIElements.StyleVariableResolver;
 
 namespace RedirectLog
 {
-    [BepInPlugin("org.Nep.RedirectLog", "RedirectLog", "1.0.1")]
-    public class RedirectLog : BaseUnityPlugin
-    {
-        private ConfigEntry<bool> configEnableTimestamp;
-        private ConfigEntry<bool> configEnableFPS;
-        void Awake()
-        {
-            configEnableTimestamp = Config.Bind("General",
-                                    "Enable TimeStamps",
-                                    true,
-                                    "Whether or not to show TimeStamps in the Log");
-            configEnableFPS = Config.Bind("General",
-                        "Enable FPS Log",
-                        false,
-                        "Whether or not to show the FPS in the Log");
-            UniLog.OnLog += OnLogMessage;
-            UniLog.OnWarning += OnWarningMessage;
-            UniLog.OnError += OnErrorMessage;
-        }
+	[BepInPlugin("org.Nep.RedirectLog", "RedirectLog", "1.0.2")]
+	public class RedirectLog : BaseUnityPlugin
+	{
+		public static readonly string logFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ResoniteLogs", "ResoniteLog-" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + ".log"); private ConfigEntry<bool> configEnableTimestamp;
+		private ConfigEntry<bool> configEnableFPS;
+		private void Awake()
+		{
+			configEnableTimestamp = Config.Bind("General",
+									"Enable TimeStamps",
+									true,
+									"Whether or not to show TimeStamps in the Log");
+			configEnableFPS = Config.Bind("General",
+						"Enable FPS Log",
+						false,
+						"Whether or not to show the FPS in the Log");
+			UniLog.OnLog += OnLogMessage;
+			UniLog.OnWarning += OnWarningMessage;
+			UniLog.OnError += OnErrorMessage;
+		}
 
-        void OnLogMessage(string message)
-        {
-            string cleanedMessage = RemoveTimestampAndFPS(message);
+		private void OnLogMessage(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return;
+			
+			string cleanedMessage = RemoveTimestampAndFPS(message);
 
-            if (IsValidLog(cleanedMessage) && cleanedMessage.Contains("ResoniteModLoader") || cleanedMessage.Contains("LastModifyingUser") || message.Contains("Failed Load: Could not gather") || message.Contains("SignalR") || message.Contains("SIGNALR"))
-            {
-                FormatModLoaderLog(cleanedMessage);
-            }
-            else if (IsValidLog(cleanedMessage))
-            {
-                console.WriteLine("[UniLog] " + cleanedMessage, ConsoleColor.Gray);
-            }
-        }
+			if (IsValidLog(cleanedMessage) && MatchesLogPatterns(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] " + message);
+				FormatModLoaderLog(cleanedMessage);
+			}
+			else if (IsValidLog(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] " + message);
+				console.WriteLine("[UniLog] " + cleanedMessage, ConsoleColor.Gray);
+			}
+		}
 
-        void OnWarningMessage(string message)
-        {
-            string cleanedMessage = RemoveTimestampAndFPS(message);
+		private void OnWarningMessage(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return;
+			
+			string cleanedMessage = RemoveTimestampAndFPS(message);
 
-            if (IsValidLog(cleanedMessage) && cleanedMessage.Contains("ResoniteModLoader"))
-            {
-                FormatModLoaderLog(cleanedMessage);
-            }
-            else if (IsValidLog(cleanedMessage))
-            {
-                console.WriteLine("[UniLog] [Warning] " + cleanedMessage, ConsoleColor.Yellow);
-            }
-        }
+			if (IsValidLog(cleanedMessage) && MatchesLogPatterns(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] [Warning] " + message);
+				FormatModLoaderLog(cleanedMessage);
+			}
+			else if (IsValidLog(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] [Warning] " + message);
+				console.WriteLine("[UniLog] [Warning] " + cleanedMessage, ConsoleColor.Yellow);
+			}
+		}
 
-        void OnErrorMessage(string message)
-        {
-            string cleanedMessage = RemoveTimestampAndFPS(message);
+		private void OnErrorMessage(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return;
+			
+			string cleanedMessage = RemoveTimestampAndFPS(message);
 
-            if (IsValidLog(cleanedMessage) && cleanedMessage.Contains("ResoniteModLoader"))
-            {
-                FormatModLoaderLog(cleanedMessage);
-            }
-            else if (IsValidLog(cleanedMessage))
-            {
-                console.WriteLine("[UniLog] [Error] " + cleanedMessage, ConsoleColor.Red);
-            }
-        }
+			if (IsValidLog(cleanedMessage) && MatchesLogPatterns(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] [Error] " + message);
+				FormatModLoaderLog(cleanedMessage);
+			}
+			else if (IsValidLog(cleanedMessage))
+			{
+				console.LogToFile("[UniLog] [Error] " + message);
+				console.WriteLine("[UniLog] [Error] " + cleanedMessage, ConsoleColor.Red);
+			}
+		}
 
-        void FormatModLoaderLog(string message)
-        {
-            if (message.Contains("[INFO]"))
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.Green);
-            }
-            else if (message.Contains("[DEBUG]"))
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.Blue);
-            }
-            else if (message.Contains("[ERROR]"))
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.Red);
+		private void FormatModLoaderLog(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return;
 
-            }
-            else if (message.Contains("[WARN]"))
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.Yellow);
-            }
-            else if (message.Contains("LastModifyingUser"))
-            {
-                console.WriteLine("[UniLog] [Warning] " + message, ConsoleColor.Yellow);
-            }
-            else if (message.Contains("Failed Load: Could not gather"))
-            {
-                console.WriteLine("[UniLog] [Error] " + message, ConsoleColor.Red);
-            }
-            else if (message.Contains("SignalR") || message.Contains("SIGNALR"))
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.DarkMagenta);
-            }
-            else
-            {
-                console.WriteLine("[UniLog] " + message, ConsoleColor.Gray);
-            }
-        }
+			var lower = message.ToLower();
+			var consoleColor = Regex.IsMatch(lower, @"\[info\]") ? ConsoleColor.Green :
+							   Regex.IsMatch(lower, @"\[debug\]") ? ConsoleColor.Blue :
+							   Regex.IsMatch(lower, @"\[error\]") ? ConsoleColor.Red :
+							   Regex.IsMatch(lower, @"\[warn\]|updated: https") ? ConsoleColor.Yellow :
+							   Regex.IsMatch(lower, @"lastmodifyinguser") ? ConsoleColor.Yellow :
+							   Regex.IsMatch(lower, @"failed load: could not gather") ? ConsoleColor.Red :
+							   Regex.IsMatch(lower, @"signalr|clearing expired status|status before clearing:|status after clearing:") ? ConsoleColor.DarkMagenta :
+							   Regex.IsMatch(lower, @"sendstatustouser:") ? ConsoleColor.Magenta :
+							   ConsoleColor.Gray;
 
-        string RemoveTimestampAndFPS(string message)
-        {
-            string pattern1 = @"\d{1,2}:\d{1,2}:\d{1,2} [APap][Mm]\.\d{1,3}\s";
-            string pattern2 = @"\(\s*-*\d+\s?FPS\s?\)\s+";
+			console.WriteLine($"[UniLog] {message}", consoleColor);
+		}
 
-            if (!configEnableTimestamp.Value)
-            {
-                string result = Regex.Replace(message, pattern1, "");
+		private bool MatchesLogPatterns(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return false;
+			
+			var lower = message.ToLower();
+			return Regex.IsMatch(lower, @"\[info\]") ||
+				   Regex.IsMatch(lower, @"\[debug\]") ||
+				   Regex.IsMatch(lower, @"\[error\]") ||
+				   Regex.IsMatch(lower, @"\[warn\]|updated: https") ||
+				   Regex.IsMatch(lower, @"lastmodifyinguser") ||
+				   Regex.IsMatch(lower, @"failed load: could not gather") ||
+				   Regex.IsMatch(lower, @"signalr|clearing expired status|status before clearing:|status after clearing:") ||
+				   Regex.IsMatch(lower, @"sendstatustouser:");
+		}
 
-                if (!configEnableFPS.Value)
-                {
-                    string result2 = Regex.Replace(result, pattern2, "");
+		private bool IsValidLog(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return false;
 
-                    return result2;
-                }
-                else return result;
-            }
-            else
-            {
-                if (!configEnableFPS.Value)
-                {
-                    string result2 = Regex.Replace(message, pattern2, "");
+			string lower = message.ToLower();
+			return !Regex.IsMatch(lower, "session updated, forcing status update") &&
+				   !Regex.IsMatch(lower, @"\[debug\]\[resonitemodloader\] intercepting call to appdomain\.getassemblies\(\)") &&
+				   !Regex.IsMatch(lower, "rebuild:");
+		}
 
-                    return result2;
-                }
-                else return message;
-            }
-        }
+		private string RemoveTimestampAndFPS(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return null;
 
-        bool IsValidLog(string message)
-        {
-            return !string.IsNullOrWhiteSpace(message) &&
-                   !message.Contains("Session updated, forcing status update") &&
-                   !message.Contains("[DEBUG][ResoniteModLoader] Intercepting call to AppDomain.GetAssemblies()") &&
-                   !message.Contains("Rebuild:");
-        }
-    }
+			string timestampPattern = @"\d{1,2}:\d{1,2}:\d{1,2} [APap][Mm]\.\d{1,3}\s";
+			string fpsPattern = @"\(\s*-*\d+\s?FPS\s?\)\s+";
+
+			if (!configEnableTimestamp.Value)
+			{
+				message = Regex.Replace(message, timestampPattern, "");
+			}
+
+			if (!configEnableFPS.Value)
+			{
+				message = Regex.Replace(message, fpsPattern, "");
+			}
+
+			return message;
+		}
+	}
+	
+	public class console 
+	{
+		public static void WriteLine(string text, ConsoleColor consoleColor)
+		{
+			ConsoleManager.SetConsoleColor(consoleColor);
+			ConsoleManager.StandardOutStream.WriteLine(text);
+		}
+
+		public static void Write(string text, ConsoleColor consoleColor)
+		{
+			ConsoleManager.SetConsoleColor(consoleColor);
+			ConsoleManager.StandardOutStream.Write(text);
+		}
+
+		public static void LogToFile(string message)
+		{
+			if (!string.IsNullOrWhiteSpace(message)) return;
+
+			string logDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ResoniteLogs");
+
+			if (!Directory.Exists(logDirectory))
+			{
+				Directory.CreateDirectory(logDirectory);
+			}
+
+			File.AppendAllText(RedirectLog.logFilePath, message + Environment.NewLine);
+		}
+	}
 }
--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 #### A Plugin for [BepInEx](https://github.com/BepInEx/BepInEx/) that is made to redirect the logs of the game [Resonite](https://store.steampowered.com/app/2519830/Resonite/) to the BepInEx Console
 
+### Note you might have issues with some select mods when installing BepInEx.
+
 ### Installation
  Download and extract [BepInEx 5.4.22 x64](https://github.com/BepInEx/BepInEx/releases) into the main game folder and run the game once to generate the folders and config files
 
- You will also need to download [BCE](https://github.com/innominata/BepInEx-Console-Extensions/releases/tag/1.0) (BepInEx Console Extensions) and place this into the "GameDir"/BepInEx/plugins
+> BCE is no longer needed after Version 1.0.2!
+
+ ~~You will also need to download [BCE](https://github.com/innominata/BepInEx-Console-Extensions/releases/tag/1.0) (BepInEx Console Extensions) and place this into the "GameDir"/BepInEx/plugins~~
 
  Finally download the RedirectLog.dll from the [Releases](https://github.com/nepushiro/RedirectLog/releases) and place this into the "GameDir"/BepInEx/plugins
 
